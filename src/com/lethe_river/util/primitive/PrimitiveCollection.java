ee9d68b7df8e1aa57d2485f987c719e285a8f7c4
YuyaAizawa <yaizawa@sa.cs.titech.ac.jp>
2018-10-25 16:17:45 +0900

    ドキュメント PrimitiveCollection

--- src/com/lethe_river/util/primitive/PrimitiveCollection.java
+++ src/com/lethe_river/util/primitive/PrimitiveCollection.java
@@ -6,8 +6,7 @@ import java.util.Collection;
 /**
  * プリミティブを格納するCollection
  *
- * 実装する場合は対応するプリミティブTに対して以下のメソッドを実装すること
- * boolean contains(T t)
+ * 実装する場合は対応するプリミティブTに対して以下のメソッドを実装すること boolean contains(T t)
  *
  * @author YuyaAizawa
  *
@@ -19,6 +18,7 @@ public interface PrimitiveCollection<T> extends Serializable {
 	 * 指定されたオブジェクトとこのコレクションが等しいかどうかを比較する.
 	 * 対称性に関する規約に基づき，java.util.Collectionとの比較結果は必ずfalseとなる．
 	 * 比較が必要な場合は{@link PrimitiveCollection#boxedView()}で取得したインスタンスを利用せよ．
+	 *
 	 * @return 等しければtrue
 	 */
 	@Override
@@ -26,13 +26,14 @@ public interface PrimitiveCollection<T> extends Serializable {
 
 	/**
 	 * このコレクションが空ならtrueを返す.
+	 *
 	 * @return 空ならtrue
 	 */
 	boolean isEmpty();
 
 	/**
-	 * このコレクションの要素数を返す.
-	 * 要素数がInteger.MAX_VALUEより大きい場合はInteger.MAX_VALUEを返す.
+	 * このコレクションの要素数を返す. 要素数がInteger.MAX_VALUEより大きい場合はInteger.MAX_VALUEを返す.
+	 *
 	 * @return 要素数
 	 */
 	int size();
@@ -43,10 +44,9 @@ public interface PrimitiveCollection<T> extends Serializable {
 	void clear();
 
 	/**
-	 * ラッパー型のCollectionのビューを返す.
-	 * ビューに対する操作はこのインスタンスに反映される．
+	 * ラッパー型のCollectionのビューを返す. ビューに対する操作はこのインスタンスに反映される．
+	 *
 	 * @return このPrimitiveCollectionのラッパー型のビュー
 	 */
 	Collection<T> boxedView();
 }
-
